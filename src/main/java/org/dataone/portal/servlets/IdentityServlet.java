14c6abc4b7759cebcc34dad6cd1fbcb28a2bfd00
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2012-12-11 22:26:41 +0000

    nope, need to force UTF-8 request encoding. https://redmine.dataone.org/issues/3416

--- src/main/java/org/dataone/portal/servlets/IdentityServlet.java
+++ src/main/java/org/dataone/portal/servlets/IdentityServlet.java
@@ -88,11 +88,12 @@ public class IdentityServlet extends HttpServlet {
 		Subject subject = null;
 		X509Certificate certificate = null;
 		
-		// can we set this?
-		//request.setCharacterEncoding("UTF-8");
+		// force UTF-8
+		// see https://redmine.dataone.org/issues/3416
+		request.setCharacterEncoding("UTF-8");
 
-		//log.debug("request characterEncoding: " + request.getCharacterEncoding());
-		System.out.println("request characterEncoding: " + request.getCharacterEncoding());
+		log.debug("request characterEncoding: " + request.getCharacterEncoding());
+		//System.out.println("request characterEncoding: " + request.getCharacterEncoding());
 
 		// get the certificate, if we have it
 		try {
@@ -140,14 +141,14 @@ public class IdentityServlet extends HttpServlet {
 	    		// gather the information needed for this method
 		    	Person person = new Person();
 		    	String familyName = request.getParameter("familyName");
-				//log.debug("familyName: " + familyName);
-				System.out.println("familyName: " + familyName);
+				log.debug("familyName: " + familyName);
+				//System.out.println("familyName: " + familyName);
 		    	String givenName = request.getParameterValues("givenName")[0];
-				//log.debug("givenName: " + givenName);
-		    	System.out.println("givenName: " + givenName);
+				log.debug("givenName: " + givenName);
+		    	//System.out.println("givenName: " + givenName);
 		    	String email = request.getParameterValues("email")[0];
-				//log.debug("email: " + email);
-				System.out.println("email: " + email);
+				log.debug("email: " + email);
+				//System.out.println("email: " + email);
 				person.setFamilyName(familyName);
 				person.addEmail(email);
 				person.addGivenName(givenName);
