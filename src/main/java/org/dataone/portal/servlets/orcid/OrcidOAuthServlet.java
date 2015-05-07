a93a1ee8a96e6aeffd6b0269042b0903684e600f
Ben Leinfelder <leinfelder@nceas.ucsb.edu>
2015-05-07 21:38:08 +0000

    consolidate oauth+hz features into a singleton to be shared across authentication servlets - hope is to allow auth via CILogon and ORCID both resulting in token from the /portal/token endpoint

--- src/main/java/org/dataone/portal/servlets/orcid/OrcidOAuthServlet.java
+++ src/main/java/org/dataone/portal/servlets/orcid/OrcidOAuthServlet.java
@@ -23,20 +23,15 @@
 package org.dataone.portal.servlets.orcid;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.URLConnectionClient;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
@@ -46,49 +41,22 @@ import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
 import org.apache.oltu.oauth2.common.message.types.ResponseType;
-import org.dataone.portal.TokenGenerator;
-
-import com.hazelcast.client.ClientConfig;
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.config.FileSystemXmlConfig;
-import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
-import com.nimbusds.jose.JOSEException;
+import org.dataone.portal.oauth.OAuthHelper;
 
 /**
  * Simple servlet for handling ORCID auth
  */
 public class OrcidOAuthServlet extends HttpServlet {
 	
-	private IMap<String, Map<String, Object>> sessions = null;
-
 	private static final String AUTHORIZATION_LOCATION = "https://sandbox.orcid.org/oauth/authorize";
 	private static final String TOKEN_LOCATION = "https://api.sandbox.orcid.org/oauth/token";
 	private static final String REDIRECT_URI = "https://cn-sandbox-2.test.dataone.org/portal/oauth";
 	private static final String CLIENT_ID = "APP-YLSPZFL1W1JVKOXX";
 	private static final String CLIENT_SECRET = "6cb791cc-8cfd-413c-8717-2be3bffa75e8";
 
-	
 	public void init(ServletConfig config) throws ServletException {
 		
-		try {
-			// connect to HZ cluster as client for sharing sessions
-			String configFileName = config.getInitParameter("config-location");
-			FileSystemXmlConfig hzConfig = new FileSystemXmlConfig(configFileName);
-			String hzGroupName = hzConfig.getGroupConfig().getName();
-	        String hzGroupPassword = hzConfig.getGroupConfig().getPassword();
-	        String hzAddress = hzConfig.getNetworkConfig().getInterfaces().getInterfaces().iterator().next() + ":" + hzConfig.getNetworkConfig().getPort();
-	        ClientConfig cc = new ClientConfig();
-	        cc.getGroupConfig().setName(hzGroupName);
-	        cc.getGroupConfig().setPassword(hzGroupPassword);
-	        cc.addAddress(hzAddress);
-			HazelcastInstance hzInstance = HazelcastClient.newHazelcastClient(cc);
-			String sessionMapName = config.getInitParameter("map-name");
-			sessions = hzInstance.getMap(sessionMapName);
-			
-		} catch (Exception e) {
-			throw new ServletException(e);
-		}
+		OAuthHelper.getInstance().init(config);
 		
 	}
 	
@@ -101,10 +69,7 @@ public class OrcidOAuthServlet extends HttpServlet {
 		try {
 			String action = request.getParameter("action");
 			if (action != null) {
-				if (action.equals("token")) {
-					this.handleGetToken(request, response);
-				}
-				else if (action.equals("start")) {
+				if (action.equals("start")) {
 					this.handleStart(request, response);
 				}
 			}
@@ -120,24 +85,12 @@ public class OrcidOAuthServlet extends HttpServlet {
 	
 	private void handleStart(HttpServletRequest request, HttpServletResponse response) throws OAuthSystemException, IOException {
 		
-		
-		
 		// remember for the callback
 		HttpSession session = request.getSession();
 		// where should we end up with afterward?
 		String target = request.getParameter("target");
 		session.setAttribute("target", target);
-		if (!sessions.containsKey(session.getId())) {
-			// save the session info in a shared map
-			Map<String, Object> sessionMap = new HashMap<String, Object>();
-			Enumeration attributeNames = session.getAttributeNames();
-			while (attributeNames.hasMoreElements()) {
-				String name = (String) attributeNames.nextElement();
-				Object value = session.getAttribute(name);
-				sessionMap.put(name, value);
-			}
-			sessions.put(session.getId(), sessionMap );
-		}
+		OAuthHelper.getInstance().saveSession(session);
 		
 		OAuthClientRequest oauthRequest = OAuthClientRequest
 				   .authorizationLocation(AUTHORIZATION_LOCATION)
@@ -188,43 +141,23 @@ public class OrcidOAuthServlet extends HttpServlet {
 		String orcid = oAuthResponse.getParam("orcid");
 		String name = oAuthResponse.getParam("name");
 		
-		Map<String, Object> sessionMap = sessions.get(sessionId);
+		Map<String, Object> sessionMap = OAuthHelper.getInstance().getMap(sessionId);
 		sessionMap.put("accessToken", accessToken);
 		sessionMap.put("expiresIn", expiresIn);
 		sessionMap.put("scope", scope);
 		sessionMap.put("orcid", orcid);
 		sessionMap.put("name", name);
-		sessions.put(sessionId, sessionMap);
+		OAuthHelper.getInstance().saveMap(sessionId, sessionMap);
 		
 		String target = (String) sessionMap.get("target");
 		if (target != null) {
 			// redirect to target
 			response.sendRedirect(target);
 		} else {
-			// redirect to token
-			response.sendRedirect(REDIRECT_URI + "?action=token");
+			// redirect to token context base?
+			response.sendRedirect(this.getServletContext().getResource("/").toString());
 		}
 
-	}
-	
-	private void handleGetToken(HttpServletRequest request, HttpServletResponse response) throws IOException, JOSEException, ParseException {
-		
-		// look up the token
-		HttpSession session = request.getSession();
-		Map<String, Object> sessionMap = sessions.get(session.getId());
-		String accessToken = (String) sessionMap.get("accessToken");
-		String orcid = (String) sessionMap.get("orcid");
-		String name = (String) sessionMap.get("name");
-		
-		String jwt = "";
-		if (accessToken != null) {
-			jwt = TokenGenerator.getInstance().getJWT(orcid, name);
-		}
-		
-		// write the JWT token
-		ServletOutputStream out = response.getOutputStream();
-		IOUtils.write(jwt, out);
-	
 	}
 
 }
